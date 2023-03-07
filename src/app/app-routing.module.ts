304846fa84194c85fcfecea2d75edb17d1c41e4e
Smule (Samuel Runmark Thunell) <samuel.runmark@gmail.com>
2023-03-07 01:03:13 +0100

    Routing enabled with the resume page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'resume', component: ResumeComponent},
+
+  // Else
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
